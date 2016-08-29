68f5a173c5613f1819fd8a7e5e5b813f9a4221be
John McAlmon 2014 <jamcalmon@gmail.com>
2016-08-29 18:56:12 -0500

    Fix 404 handler bug.

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 import os
-import flask
+from flask import request, session, abort, g, render_template, redirect, flash, jsonify, url_for
 import bcrypt
 import base64
 from datetime import datetime, timedelta
@@ -10,36 +10,36 @@ import models
 
 @app.before_request
 def csrf_protect():
-    if flask.request.method == "POST":
-        if 'csrf_token' in flask.session:
-            if flask.request.form['csrf_token'] != flask.session['csrf_token']:
-                flask.abort(403)
+    if request.method == "POST":
+        if 'csrf_token' in session:
+            if request.form['csrf_token'] != session['csrf_token']:
+                abort(403)
         else:
-            flask.abort(403)
+            abort(403)
 
 
 @app.before_request
 def setup_csrf_token():
-    if 'csrf_token' not in flask.session:
-        flask.session['csrf_token'] = base64.b64encode(os.urandom(32)).decode('ascii')
+    if 'csrf_token' not in session:
+        session['csrf_token'] = base64.b64encode(os.urandom(32)).decode('ascii')
 
 
 @app.before_request
 def setup_user_global():
-    if 'auth_user' in flask.session:
-        user = models.User.query.get(flask.session['auth_user'])
+    if 'auth_user' in session:
+        user = models.User.query.get(session['auth_user'])
         if user is None:
-            del flask.session['auth_user']
-        flask.g.user = user
+            del session['auth_user']
+        g.user = user
 
 ### Webpage handlers ###
 
 @app.route('/')
 def main_page():
     # If user is logged in, render the main activity page
-    if 'auth_user' in flask.session:
+    if 'auth_user' in session:
         # Check if user has activity underway
-        uid = flask.session['auth_user']
+        uid = session['auth_user']
         activity_check = models.Activity.query.filter_by(user_id=uid).filter_by(current=True).first()
         in_progress = 0
         description = ''
@@ -48,27 +48,27 @@ def main_page():
             in_progress = 1
             description = activity_check.description
             start_time = activity_check.start_time.isoformat()
-        return flask.render_template('activity.html', in_progress=in_progress, description=description, start_time=start_time)
+        return render_template('activity.html', in_progress=in_progress, description=description, start_time=start_time)
     # User is not logged in; render login page
-    return flask.render_template('index.html')
+    return render_template('index.html')
 
 
 @app.errorhandler(404)
-def page_not_found():
-    return flask.render_template('not_found.html'), 404
+def page_not_found(error):
+    return render_template('not_found.html'), 404
 
 ### API handlers ###
 
 @app.route('/start-activity', methods=['POST'])
 def start_activity():
-    description = flask.request.form['description']
-    uid = flask.request.form['user_id']
+    description = request.form['description']
+    uid = request.form['user_id']
     if not check_auth(uid):
-        return flask.abort(403)
+        return abort(403)
     # Check if user already has an activity underway
     activity_check = models.Activity.query.filter_by(user_id=uid).filter_by(current=True).first()
     if activity_check is not None:
-        flask.abort(400)
+        abort(400)
     # Create activity object
     activity =  models.Activity()
     activity.description = description
@@ -83,19 +83,19 @@ def start_activity():
     json_activity = {}
     json_activity['description'] = activity.description
     json_activity['start'] = start_time
-    return flask.jsonify(json_activity)
+    return jsonify(json_activity)
 
 
 @app.route('/end-activity', methods=['POST'])
 def end_activity():
-    uid = flask.request.form['user_id']
+    uid = request.form['user_id']
     if not check_auth(uid):
-        return flask.abort(403)
+        return abort(403)
     activity = models.Activity.query.filter_by(user_id=uid).order_by(models.Activity.id.desc()).first()
     if activity is None:
-        flask.abort(400)
+        abort(400)
     if activity.current is False:
-        flask.abort(400)
+        abort(400)
     activity.current = False
     activity.end_time = datetime.utcnow()
     db.session.commit()
@@ -104,10 +104,10 @@ def end_activity():
 
 @app.route('/get-activities', methods=['POST'])
 def get_activities():
-    uid = flask.request.form['user_id']
+    uid = request.form['user_id']
     if not check_auth(uid):
-        return flask.abort(403)
-    index = int(flask.request.form['index'])
+        return abort(403)
+    index = int(request.form['index'])
     index *= 10
     activities = models.Activity.query.filter_by(user_id=uid).filter_by(current=False).\
         order_by(models.Activity.id.desc()).limit(10).offset(index).all()
@@ -127,7 +127,7 @@ def get_activities():
             order_by(models.Activity.id.desc()).offset(index+10).first()
         if next_check is not None:
             has_next = 1
-    return flask.jsonify({'activities': activity_list, 'has_next': has_next})
+    return jsonify({'activities': activity_list, 'has_next': has_next})
 
 
 ### Authentication handlers ###
@@ -135,32 +135,32 @@ def get_activities():
 
 @app.route('/login', methods=['POST'])
 def login():
-    username = flask.request.form['username']
-    password = flask.request.form['password']
+    username = request.form['username']
+    password = request.form['password']
     user = models.User.query.filter_by(username=username).first()
     if user is not None:
     # Hash the password given in the form and compare to the stored hash for this user
         pw_hash = bcrypt.hashpw(password.encode('utf8'), user.pw_hash)
         if pw_hash == user.pw_hash:
-            flask.session['auth_user'] = user.id
-            return flask.redirect(flask.url_for('main_page'), 303) # change this later
+            session['auth_user'] = user.id
+            return redirect(url_for('main_page'), 303) # change this later
     # If username or password is invalid, redirect to main page
-    flask.flash('Invalid username or password.')
-    return flask.redirect(flask.url_for('main_page'))
+    flash('Invalid username or password.')
+    return redirect(url_for('main_page'))
 
 
 @app.route('/register', methods=['POST'])
 def create_account():
-    username = flask.request.form['username']
-    password = flask.request.form['password']
-    pwConfirm = flask.request.form['password-confirm']
+    username = request.form['username']
+    password = request.form['password']
+    pwConfirm = request.form['password-confirm']
     username_check = models.User.query.filter_by(username=username).first()
     if username_check is not None:
-        flask.flash('There is already an account registered for this username address.')
-        return flask.redirect(flask.url_for('main_page'), 303)
+        flash('There is already an account registered for this username address.')
+        return redirect(url_for('main_page'), 303)
     if password != pwConfirm:
-        flask.flash('Passwords do not match.')
-        return flask.redirect(flask.url_for('main_page'), 303)
+        flash('Passwords do not match.')
+        return redirect(url_for('main_page'), 303)
     # Create a new User object
     user = models.User()
     user.username = username
@@ -169,22 +169,22 @@ def create_account():
     db.session.add(user)
     db.session.commit()
     #Save user info in Flask session
-    flask.session['auth_user'] = user.id
-    flask.g.user = user
-    return flask.redirect(flask.url_for('main_page'), 303) # change this later
+    session['auth_user'] = user.id
+    g.user = user
+    return redirect(url_for('main_page'), 303) # change this later
 
 
 @app.route('/logout', methods=['POST'])
 def logout():
-    del flask.session['auth_user']
-    del flask.g.user
-    return flask.redirect(flask.url_for('main_page'), 303)
+    del session['auth_user']
+    del g.user
+    return redirect(url_for('main_page'), 303)
 
 
 # Checks if user is properly authenticated.
 def check_auth(uid):
-    if 'auth_user' not in flask.session:
+    if 'auth_user' not in session:
         return False
-    if int(uid) is not flask.session['auth_user']:
+    if int(uid) is not session['auth_user']:
         return False
     return True
